40c0e253d0314c02f8b7f94f61f4b689e70ddece
skad27 <adam.pezz@hotmail.com>
2024-12-01 16:55:16 +0000

    ci : test

--- MyProjFolder/test/test_snapshot.py
+++ MyProjFolder/test/test_snapshot.py
@@ -3,18 +3,10 @@ import pytest
 import json
 from mymodule import get_ld50
 
-def test_pubchem_ld50(snapshot):
-    # Creazione della sessione di rete
+
+def test_pubchem_snapshot(snapshot):
     session = requests.Session()
-
-    # Utilizzo di un altro CID, ad esempio "1983" (caffeina)
-    cid = "2244"
-
-    # Recupero dei valori LD50
-    ld50_data = get_ld50(session, cid)
-
-    # Serializzazione in JSON per confronto
-    serialized_ld50 = json.dumps(ld50_data, indent=4)
-
-    # Confronto con lo snapshot fornito
-    snapshot.assert_match(serialized_ld50, 'pubchem_ld50_snapshot')
+    cid = "2244"  # CID per l'Aspirina
+    ld50_values = get_ld50(session, cid)
+    serialized_data = json.dumps(ld50_values, indent=4)
+    snapshot.assert_match(serialized_data, 'pubchem_ld50_snapshot')
